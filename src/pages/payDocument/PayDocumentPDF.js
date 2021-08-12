b0c176fa7612905c32c4895594a94e578cb5c026
kkyubrother@naver.com <Lun@ria1024E420>
2021-08-12 18:31:16 +0900

    pdf 데이터 추가

--- src/pages/payDocument/PayDocumentPDF.js
+++ src/pages/payDocument/PayDocumentPDF.js
@@ -90,9 +90,9 @@ class PayDocumentPDF extends Component {
                             <View><Text>기타수당(비과세)</Text></View>
                         </View>
                         <View>
-                            <View><Text>기본급</Text></View>
-                            <View><Text>기타수당(과세)</Text></View>
-                            <View><Text>기타수당(비과세)</Text></View>
+                            <View><Text>{this.props.data.salary}</Text></View>
+                            <View><Text>{this.props.data.taxation}</Text></View>
+                            <View><Text>{this.props.data.taxFree}</Text></View>
                         </View>
                     </View>
                     <View style={styles.bodyTax}>
@@ -108,22 +108,22 @@ class PayDocumentPDF extends Component {
                             <View><Text>주민세</Text></View>
                         </View>
                         <View>
-                            <View><Text>국민연금</Text></View>
-                            <View><Text>건강보험료</Text></View>
-                            <View><Text>장기요양보험료</Text></View>
-                            <View><Text>고용보험료</Text></View>
+                            <View><Text>{this.props.data.nationalPension}</Text></View>
+                            <View><Text>{this.props.data.healthInsurance}</Text></View>
+                            <View><Text>{this.props.data.regularCare}</Text></View>
+                            <View><Text>{this.props.data.employmentInsurance}</Text></View>
                             <View><Text>소득세</Text></View>
                             <View><Text>주민세</Text></View>
                         </View>
                     </View>
                     <View style={styles.footer}>
-                        <Text style={styles.item}>지급액계</Text><Text style={styles.item}>-</Text>
+                        <Text style={styles.item}>지급액계</Text><Text style={styles.item}>{this.props.data.origin}</Text>
                     </View>
                     <View style={styles.footer}>
-                        <Text style={styles.item}>공제액계</Text><Text style={styles.item}>-</Text>
+                        <Text style={styles.item}>공제액계</Text><Text style={styles.item}>{this.props.data.minus}</Text>
                     </View>
                     <View style={styles.footer}>
-                    <Text style={styles.item}>차인지급액계</Text><Text style={styles.item}>-</Text>
+                    <Text style={styles.item}>차인지급액계</Text><Text style={styles.item}>{this.props.data.real}</Text>
                     </View>
                 </Page>
             </Document>
