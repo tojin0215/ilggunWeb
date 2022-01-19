822f065c12a07f886b28f314443d5ebed2995007
perust <seungwoo3859@gmail.com>
2022-01-19 15:29:51 +0900

    UI modify 011902

--- src/components/Navigation/Table2.js
+++ src/components/Navigation/Table2.js
@@ -2,32 +2,37 @@ import React, { useMemo } from 'react';
 
 import DataTable from 'react-data-table-component';
 import FilterComponent from './FilterComponent';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
 
-
-const Table = props => {
+const Table = (props) => {
   const { openModal, data, deleteWorker } = props;
   const columns = [
     {
-      name: "이름",
+      name: '이름',
       selector: (row, index) => row.workername2,
-      sortable: true
+      sortable: true,
     },
     {
-      name: "QR",
+      name: 'QR',
       Button: true,
-      cell: row =>
-        (row.state == 2) ? (
+      cell: (row) =>
+        row.state == 2 ? (
           <>
-            <Button onClick={() => openModal(row.workername)}> QR </Button>
+            <Button
+              className="w-100 fw-bold text-h6"
+              onClick={() => openModal(row.workername)}
+            >
+              {' '}
+              QR{' '}
+            </Button>
           </>
-        ) : null
+        ) : null,
     },
 
     {
-      name: "입사일",
+      name: '입사일',
       selector: (row, index) => row.startdate,
-      sortable: true
+      sortable: true,
     },
 
     // {
@@ -36,43 +41,55 @@ const Table = props => {
     //   sortable: true
     // },
     {
-      name: "퇴직처리",
+      name: '퇴직처리',
       Button: true,
-      cell: row =>
-        (row.state == 2) ? (
+      cell: (row) =>
+        row.state == 2 ? (
           <>
-            <Button variant="danger" onClick={() => deleteWorker(null, row.workername)}>퇴사</Button>
+            <Button
+              className="py-1 fw-bold"
+              variant="danger"
+              onClick={() => deleteWorker(null, row.workername)}
+            >
+              퇴사
+            </Button>
           </>
-        )
-          : (row.state == 0) ?
-            (<span>미입사</span>)
-            : <><span>{(row.retire_date) ? row.retire_date : new Date().toLocaleDateString()} 퇴사 </span></>
+        ) : row.state == 0 ? (
+          <span>미입사</span>
+        ) : (
+          <>
+            <span>
+              {row.retire_date
+                ? row.retire_date
+                : new Date().toLocaleDateString()}{' '}
+              퇴사{' '}
+            </span>
+          </>
+        ),
     },
   ];
 
-  const [filterText, setFilterText] = React.useState("");
-  const [resetPaginationToggle, setResetPaginationToggle] = React.useState(
-    false
-  );
+  const [filterText, setFilterText] = React.useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] =
+    React.useState(false);
   // const filteredItems = data.filter(
   //   item => item.name && item.name.includes(filterText)
   // );
   const filteredItems = props.data.filter(
-    item =>
-      item.workername2.indexOf(filterText) !== -1
+    (item) => item.workername2.indexOf(filterText) !== -1,
   );
 
   const subHeaderComponent = useMemo(() => {
     const handleClear = () => {
       if (filterText) {
         setResetPaginationToggle(!resetPaginationToggle);
-        setFilterText("");
+        setFilterText('');
       }
     };
 
     return (
       <FilterComponent
-        onFilter={e => setFilterText(e.target.value)}
+        onFilter={(e) => setFilterText(e.target.value)}
         onClear={handleClear}
         filterText={filterText}
       />
